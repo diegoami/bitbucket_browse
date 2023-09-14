3f985ba37beb24f8fd18e9e7a6babd7d3a7f1527
Diego Amicabile <diego.amicabile@gmail.com>
2023-09-14 21:47:15 +0200

    get projects too

--- connect_bbucket.py
+++ connect_bbucket.py
@@ -15,38 +15,52 @@ workspaces_response = requests.get(
 
 if workspaces_response.status_code == 200:
     workspaces = workspaces_response.json()['values']
-
     all_repositories = []
 
-    # Loop through each workspace and list its repositories
+    # Loop through each workspace
     for workspace in workspaces:
         workspace_slug = workspace['slug']
 
-        repos_response = requests.get(
-            f"https://api.bitbucket.org/2.0/repositories/{workspace_slug}",
+        # Fetch projects in each workspace
+        projects_response = requests.get(
+            f"https://api.bitbucket.org/2.0/workspaces/{workspace_slug}/projects",
             auth=(username, app_password)
         )
 
-        if repos_response.status_code == 200:
-            repositories = repos_response.json()['values']
+        if projects_response.status_code == 200:
+            projects = projects_response.json()['values']
 
-            for repo in repositories:
-                all_repositories.append({
-                    'workspace': workspace_slug,
-                    'repository_name': repo['name']
-                })
-        else:
-            print(f"Failed to fetch repositories for workspace {workspace_slug}")
+            # Loop through each project in the workspace
+            for project in projects:
+                project_name = project['name']
+                project_key = project['key']
+
+                # Fetch repositories in each project
+                repos_response = requests.get(
+                    f"https://api.bitbucket.org/2.0/repositories/{workspace_slug}?q=project.key=\"{project_key}\"",
+                    auth=(username, app_password)
+                )
+
+                if repos_response.status_code == 200:
+                    repositories = repos_response.json()['values']
+
+                    # Loop through each repository in the project
+                    for repo in repositories:
+                        all_repositories.append({
+                            'workspace': workspace_slug,
+                            'project': project_name,
+                            'repository_name': repo['name']
+                        })
 
     # Sort all repositories by their name, case-insensitively
     sorted_repositories = sorted(all_repositories, key=lambda x: x['repository_name'].lower())
 
     # Print the header for the table
-    print(f"{'Repository':<50}{'Workspace':<30}")
+    print(f"{'Repository':<50}{'Project':<30}{'Workspace':<30}")
 
-    # Print the sorted list of repositories along with their workspaces in tabular format
+    # Print the sorted list of repositories along with their projects and workspaces
     for repo in sorted_repositories:
-        print(f"{repo['repository_name']:<60}{repo['workspace']:<30}")
+        print(f"{repo['repository_name']:<70}{repo['project']:<30}{repo['workspace']:<30}")
 
 else:
     print("Failed to fetch workspaces.")
